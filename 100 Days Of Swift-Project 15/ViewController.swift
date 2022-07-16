ae0e1d791dc4b8da58bf9a5e6631319d39a9e38e
Arda Büyükhatipoğlu <ardabho@gmail.com>
2022-07-16 15:45:31 +0300

    Day 57

--- 100 Days Of Swift-Project 15/ViewController.swift
+++ 100 Days Of Swift-Project 15/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var imageView: UIImageView!
+    var currentAnimation = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: view.frame.width / 2, y: view.frame.height / 2)
+        view.addSubview(imageView)
     }
-
-
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.isHidden = true
+        
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: []) {
+            switch self.currentAnimation {
+            case 0:
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+            case 1:
+                self.imageView.transform = .identity
+            case 2:
+                self.imageView.transform = CGAffineTransform(translationX: -200, y: -200)
+            case 3:
+                self.imageView.transform = .identity
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
+            case 5:
+                self.imageView.transform = .identity
+            case 6:
+                self.imageView.alpha = 0.1
+                self.imageView.backgroundColor = .cyan
+            case 7:
+                self.imageView.alpha = 1
+                self.imageView.backgroundColor = .clear
+            default:
+                break
+            }
+        } completion: { finished in
+            sender.isHidden = false
+        }
+        
+        
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
+    
 }
 
